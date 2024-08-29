f46338c187d1f80e3afe7ea46c25a634a5c65955
Wilher J. Polanco Eustate <86754956+wjpe27@users.noreply.github.com>
2024-08-29 14:03:40 -0400

    Add files via upload

---
+++ userService.test.ts
@@ -0,0 +1,10 @@
+import { register, authenticate, blockUser, reset } from './userService';
+describe('Fail Based Tests', () => {
+ // Desarrollar aquí pruebas Basadas en Fallas
+});
+describe('Use Based Tests', () => {
+ // Desarrollar aquí pruebas Basadas en el Uso
+});
+describe('Model Based Tests', () => {
+ // Desarrollar aquí pruebas Basadas en Modelos
+});
